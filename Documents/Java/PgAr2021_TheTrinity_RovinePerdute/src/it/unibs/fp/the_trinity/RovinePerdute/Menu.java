da4bc338b86736f469486df5511ac2cea15a6d5e
Martina Contestabile <76061932+mcontestabile@users.noreply.github.com>
2021-05-22 19:34:01 +0200

    Delete Documents/Java directory

--- Documents/Java/PgAr2021_TheTrinity_RovinePerdute/src/it/unibs/fp/the_trinity/RovinePerdute/Menu.java
+++
@@ -1,7 +0,0 @@
-package it.unibs.fp.the_trinity.RovinePerdute;
-
-public class Menu {
-    public void rovinePerduteGestion() {
-
-    }
-}
--- Documents/Java/PgAr2021_TheTrinity_TamaGolem/src/it/unibs/fp/TamaGolem/TGMain.java
+++
@@ -1,7 +0,0 @@
-package it.unibs.fp.TamaGolem;
-
-public class TGMain {
-    public static void main (String[] args) {
-        
-    }
-}
